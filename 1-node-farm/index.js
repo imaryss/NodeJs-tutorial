6a0c737b7a3b5e43da8800c0a72a8f4736cbe44a
mariaion <marianaion319@gmail.com>
2023-06-30 10:12:14 +0300

    readFileSync, reading data from external files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,2 +1,8 @@
+const fs= require('fs'); //calling this function will return an object in which there are a lot of functions that we can use
 const hello = "Hello world";
 console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); //= synchronous version of file reading
+console.log(textIn);
+// it has 2 arg. (1)- is the path to the file that we're reading
+            //   (2)- the character encoded ( if u dont specify it u get a buffer)
+            // by calling this function it will read the data from the file and return it to us
