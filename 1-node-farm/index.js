f8b1af7fab365fc06092c577b926497212a6e1ee
mariaion <marianaion319@gmail.com>
2023-06-30 10:15:12 +0300

    correcting error

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,7 @@
 const fs= require('fs'); //calling this function will return an object in which there are a lot of functions that we can use
 const hello = "Hello world";
 console.log(hello);
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); //= synchronous version of file reading
+const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8'); //= synchronous version of file reading
 console.log(textIn);
 // it has 2 arg. (1)- is the path to the file that we're reading
             //   (2)- the character encoded ( if u dont specify it u get a buffer)
